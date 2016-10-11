e705c40e2edee57aff54189ed216b16a5240a470
jeremiahwood <jzwood47@cnm.edu>
2016-10-11 13:41:08 -0600

    added text and Image

---
+++ public_html/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8"
+	</head>
+	<body>
+		<!-- This is the Header-->
+		<header>
+			<h1>Hi! This is the Best page EVER!!</h1>
+		</header>
+		<!-- this is the main page content-->
+		<main>
+
+			<section>
+				<p>
+					The Point: Your Life
+
+					What’s the point of all that? The point is not this is what you do with your loose change. The point is: This is what you do with your life. You don’t want always be sitting high in your SUV dropping nickels into other people’s dreams. Satan wants that for you. But you don’t! You want to dream your own dream for the glory of Christ. Why am I on this planet? What has God put me here for?
+
+					What broke George Verwer’s heart back in the eighties, and breaks mine today, is not mainly that you have sinned sexually, but that this morning Satan took your 2 AM encounter in the hotel room — whether on TV or in bed — and told you: “See, you’re a loser. You may as well not even go to worship. No way are you going to make any serious commitment of your life to Jesus Christ! You may as well go back to school and get a good practical education, and then a good job so you can buy yourself a big wide screen and watch sex till you drop.”
+
+					I want to take that weapon out of his hand. Yes, I want you to have the joyful courage not to even do the channel surfing. But sooner or later, whether it’s that sin or another, you are going to fall. I have come to Atlanta to help you deal with the guilt of that failure so that Satan does not use it to produce another wasted life.
+
+					Two Headings
+
+					So here is where we are going: First, we will link up with where many of your have been in Colossians in your Community Groups. And second, we will go to the prophet Micah to see what victory looks like the morning after failure.
+
+					If it will help to have two headings, here they are:
+
+					1) Theology can conquer biology. Or another way to say it more specifically: Justification can conquer fornication.
+
+					2) Trust Christ to the hilt with gutsy guilt.
+
+					1. Theology Can Conquer Biology
+
+					The backdrop of all the teaching in Colossians 1-3 is Colossians 3:6, “On account of these the wrath of God is coming.” Hanging over the whole world is the holy, just, unimpeachable anger of God at sin and rebellion. His wrath is coming and the salvation spoken of in Colossians 1-3 is rescue from that. No one wants to meet the wrath of “the Lamb” when it comes (Revelation 6:16). So God, in his mercy makes a way out.
+
+					And what is distinctively Christian about the teachings of these chapters is that our rescue was most decisively accomplished for us by another and was done outside of us. In other words, Christ did something in history before we existed that obtained and guaranteed our rescue and the transformation of all who would come to trust in him. The distinctive and crucial thing about Christian salvation is that Christ accomplished it decisively for us and outside of us and without our help. And when we put our faith in him we do not add to the sufficiency of what he accomplished in covering our sins and achieving the righteousness that counts as ours.
+
+					For You and Outside of You
+
+					The clearest verses on this point are Colossians 2:13-14, “And you, who were dead in your trespasses and the uncircumcision of your flesh, God made alive together with him, having forgiven us all our trespasses, by canceling the record of debt that stood against us with its legal demands. This he set aside, nailing it to the cross.”
+
+
+				</p>
+				<img src="img/thumb_IMG_0778_1024.jpg" alt"bible verse">
+			</section>
+			<section></section>
+			<section></section>
+
+		</main>
+	</body>
+</html>
